d6f643b691d3e2cef2508abf64131c918bf18415
Chance <cembreyfarquhar@gmail.com>
2019-08-22 22:18:19 -0500

    on to the next!

--- basic.py
+++ basic.py
@@ -1,2 +1,10 @@
 import tensorflow as tf
 
+a = tf.Variable(3, name="z")
+b = tf.Variable(14, name="y")
+f = a * b
+
+init = tf.global_variables_initializer()
+with tf.Session() as s:
+    init.run()
+    print(f.eval())
